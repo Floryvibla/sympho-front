922fb1cb02b2bc8cdf5976e2e513383b6859a709
Flory <flory.contato@gmail.com>
2024-10-19 14:03:28 -0300

    feat: Add regrar

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -49,7 +49,26 @@ export function getModel(useSubModel = false) {
   }
 
   if (googleApiKey) {
-    return google('gemini-1.5-pro-002')
+    return google('gemini-1.5-flash-latest', {
+      safetySettings: [
+        {
+          category: 'HARM_CATEGORY_HARASSMENT',
+          threshold: 'BLOCK_NONE'
+        },
+        {
+          category: 'HARM_CATEGORY_HATE_SPEECH',
+          threshold: 'BLOCK_NONE'
+        },
+        {
+          category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+          threshold: 'BLOCK_NONE'
+        },
+        {
+          category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+          threshold: 'BLOCK_NONE'
+        }
+      ]
+    })
   }
 
   if (anthropicApiKey) {
